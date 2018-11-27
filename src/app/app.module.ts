3cbd8e7f1ef6fb58f94dc7020f21466332a6c21b
kjekproject <k.jekaterynczuk@gmail.com>
2018-11-27 23:17:47 +0100

    added null guarding with *ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
-=======
 import { HttpClientModule} from '@angular/common/http';
 
->>>>>>> 7afb83309d504799e6850ce8eb97200a6a02dd33
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';

ca6b9b6476f50d1b45c6cf464a4d2fadcf2fd439
kjekproject <k.jekaterynczuk@gmail.com>
2018-11-27 20:40:59 +0100

    saved changed to weather service

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-=======
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
->>>>>>> 7afb83309d504799e6850ce8eb97200a6a02dd33
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -14,17 +9,6 @@ interface ICurrentWeatherData {
   weather: [{
     description: string,
     icon: string
-<<<<<<< HEAD
-  }],
-  main: {
-    temp: number
-  },
-  sys: {
-    country: string
-  },
-  dt: number,
-  name: string
-=======
   }];
   main: {
     temp: number
@@ -34,29 +18,19 @@ interface ICurrentWeatherData {
   };
   dt: number;
   name: string;
->>>>>>> 7afb83309d504799e6850ce8eb97200a6a02dd33
 }
 
 @Injectable({
   providedIn: 'root'
 })
-<<<<<<< HEAD
-export class WeatherService {
-=======
 export class WeatherService  implements IWeatherService {
->>>>>>> 7afb83309d504799e6850ce8eb97200a6a02dd33
 
   constructor(private httpClient: HttpClient) { }
 
   getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
-<<<<<<< HEAD
-    return this.httpClient.get<ICurrentWeatherData>(`${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` 
-      + `q=${city},${country}&appid=${environment.appId}`).pipe(map(data => this.transformToICurrentWeather(data)));
-=======
     return this.httpClient.get<ICurrentWeatherData>(`http://api.openweathermap.org/data/2.5/weather?q=` +
       `${city},${country}&appid=${environment.appId}`).pipe(map(data => this.transformToICurrentWeather(data)));
       // this returns an object  with a type of Observable<ICurrentWeatherData> //
->>>>>>> 7afb83309d504799e6850ce8eb97200a6a02dd33
   }
 
   private transformToICurrentWeather(data: ICurrentWeatherData): ICurrentWeather {
@@ -65,17 +39,6 @@ export class WeatherService  implements IWeatherService {
       country: data.sys.country,
       date: data.dt * 1000,
       image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
-<<<<<<< HEAD
-      temperature: this.convertKalvinToFahrenheit(data.main.temp),
-      description: data.weather[0].description
-    }
-  }
-
-  private convertKalvinToFahrenheit(kalvin: number): number {
-    return kalvin * 9 / 5 - 459.67;
-  }
-}
-=======
       temperature: this.convertKelvinToFahrenheint(data.main.temp),
       description: data.weather[0].description
     };
@@ -89,4 +52,3 @@ export class WeatherService  implements IWeatherService {
 export interface IWeatherService {
   getCurrentWeather(city: string, country: string): Observable<ICurrentWeather>;
 }
->>>>>>> 7afb83309d504799e6850ce8eb97200a6a02dd33
